da8cd7c8e7395ac2e054623ab2766f605ba17dac
Barandun <j.barandun@lincolninternational.ch>
2021-09-23 15:06:28 +0200

    Changes

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const https = require("https");
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.listen(3000, function() {
   console.log("server is running on port 3000");
@@ -16,31 +18,46 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
-  console.log(req.body.topicInput);
+app.post("/", function(req, res) {
+console.log(req.body.topicInput);
 
-  const query = req.body.topicInput;
-  const apiKey = "e64e32714b864219825539534c377a76"
-  const url = "https://newsapi.org/v2/everything?q=" + query +"&from=2021-09-22&to=2021-09-22&sortBy=popularity&apiKey=" + apiKey;
+const query = req.body.topicInput;
+const apiKey = "e64e32714b864219825539534c377a76"
+const url = "https://newsapi.org/v2/everything?q=" + query + "&from=2021-09-22&to=2021-09-22&sortBy=popularity&apiKey=" + apiKey;
 
-  https.get(url, function(response){
-    console.log(response.statusCode);
-    console.log(url);
+https.get(url, function(response) {
+  console.log(response.statusCode);
+  console.log(url);
 
-    response.on("data", function(data) {
-      const newsData = JSON.parse(data);
-      //
-      // for (var i = 0; i < 20; i++) {
-      //   res.write("Source: " + newsData.articles[0].source.name);
-      //   res.write("<hr>");
-      //   res.send();
+  request(url, function(error, response, body) {
+    //Check for error
+    if (error) {
+      return console.log('Error:', error);
+    }
 
-      res.send(newsData);
+    //Check for right status code
+    if (response.statusCode !== 200) {
+      return console.log('Invalid Status Code Returned:', response.statusCode);
+    }
 
-    // });
+    console.log(body); // Here is the response body
 
-    });
   });
+
+  // response.on("data", function(data) {
+  //   const newsData = JSON.parse(data);
+  //
+  // for (var i = 0; i < 20; i++) {
+  //   res.write("Source: " + newsData.articles[0].source.name);
+  //   res.write("<hr>");
+  //   res.send();
+
+  // res.send(newsData);
+
+  // });
+
+});
+});
 });
 
 // API KEY
